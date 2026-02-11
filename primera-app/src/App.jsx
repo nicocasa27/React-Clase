ac8cbf313cea6ee623fa46488b7c90aa7a16faa1
Soycloud1 <60202275+nicocasa27@users.noreply.github.com>
2026-02-10 21:02:12 -0600

    mas

--- primera-app/src/App.jsx
+++ primera-app/src/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Header from './components/Header'
 
 const App = () => {
+
+ const [items, setItems] = useState([])
+ const [isLoading, setIsLoading] = useState(true)
+
+ useEffect(() => {
+
+  const fetchItems = async () => {
+    const resultado = await axios.get('./personajes.json')
+    console.log(resultado.data)
+    setItems(resultado.data)
+
+
+
+  }
+
+
+    fetchItems()
+
+ },[])
+
   return (
+    <div className='container' >
     <Header />
+    </div>
   )
 }
 
-export default App
+export default App  
